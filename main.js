bc757d96e83e87958e74f2eb57bf07249aa2ea51
Faisal Anwar <faisaldev7@gmail.com>
2017-06-18 22:09:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -614,38 +614,7 @@
             return toReturn;
         }
 
-        $('#contact-form').on('submit', function(event) {
-
-            event.preventDefault();
-
-            requiredInputs.forEach(function(element, index) {
-                if (!$(element).val()) {
-                    $(element).parent('.form-group').addClass('has-error');
-                } else {
-                    $(element).parent('.form-group').removeClass('has-error');
-                }
-            });
-
-            if (isValidForm()) {
-                $.ajax({
-                    url: $(this).attr('action'),
-                    type: 'POST',
-                    data: $(this).serialize(),
-                }).done(function() {
-                    var message = $('#contact-form').data('success-text') || 'Your message has been sent. We will get back to you shortly!';
-                    var succesTemplate = '<div role="alert" class="alert alert-success alert-outline">' + message + '</div>';
-                    $('#contact-form input, #contact-form textarea, #contact-form button').attr('disabled', 'disabled');
-                    $('#contact-form .alert').fadeOut(300);
-                    $(succesTemplate).insertBefore($('#contact-form button'));
-                }).fail(function() {
-                    var message = $('#contact-form').data('error-text') || 'There was an error. Try again later.';
-                    var errorTemplate = '<div role="alert" class="alert alert-danger alert-outline">' + message + '</div>';
-                    $('#contact-form .alert').fadeOut(300);
-                    $(errorTemplate).insertBefore($('#contact-form button'));
-                })
-            }
-
-        });
+       
 
         $('#contact-form input, #contact-form textarea').on('keyup', function(event) {
             event.preventDefault();
